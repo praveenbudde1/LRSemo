d9a9ca77bbedd4a65383dd2db9a44996e971613b
unknown <lenovo@DESKTOP-5J9CN9B>
2019-10-11 09:17:07 +0530

    This is my first commit

---
+++ Action.c
@@ -0,0 +1,14 @@
+Action()
+{
+web_url("web_url",
+	"URL=http://lazarus/html/links.html",
+	"TargetFrame=",
+	"Resource=0",
+	"Referer=",
+	"Mode=HTTP",
+	EXTRARES,
+	"URL={extrares_11}",
+	ENDITEM,
+	LAST );	
+	return 0;
+}
